ddfe9db5f16b073291872db6d91af4a87da2b280
Eduardo Reyes <ereyesoyarzo@gmail.com>
2022-01-13 15:20:48 -0300

    card task created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Icon } from "@chakra-ui/icons"
 import { MdAccountCircle } from "react-icons/md";
-import { Flex, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react"
+import { Flex, Menu, MenuButton, MenuList, MenuItem, Heading } from "@chakra-ui/react"
 import Router from 'next/router'
 
 const Header = () => {
@@ -9,7 +9,8 @@ const Header = () => {
         Router.push('/')
     }
     return (
-        <Flex bg='#614ae0' minH={10} boxShadow={'md'} justify={'flex-end'} align={'center'}>
+        <Flex bg='#614ae0' minH={10} boxShadow={'md'} justify={'space-between'} align={'center'}>
+            <Heading ml={2} as='h3' color='white' size={'md'} fontWeight={400}>Panel de tareas</Heading>
             <Menu>
                 <MenuButton mr={2}>
                     <Icon height={6} width={6} color={'white'} as={MdAccountCircle} />
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import MainLayout from "../layouts/MainLayout";
 import Router from 'next/router'
 import Header from "../components/Header";
+import { Box, Flex, Text, Badge } from "@chakra-ui/react";
 
 const Dashboard: NextPage = () => {
 
@@ -17,7 +18,18 @@ const Dashboard: NextPage = () => {
         <>
             <MainLayout>
                 <Header></Header>
-                <h1>dashboard</h1>
+                <Box height={'100%'} mt={2} overflowY={'auto'} scrollBehavior={'auto'}>
+                    <Flex flexDirection={'row'} justify={'space-between'} shadow={'md'} minH={20} marginX={2} my={4}>
+                        <Flex flexDirection={'column'} justify={'space-evenly'} align={'baseline'} ml={2}>
+                            <Text fontSize='md' fontWeight={'bold'} textTransform={'uppercase'}>Nombre de la tarea</Text>
+                            <Badge colorScheme='green' my={2} >Completada</Badge>
+                        </Flex>
+                        <Flex direction={'column'} justify={'space-evenly'} ml={4}>
+                            <div>update</div>
+                            <div>delete</div>
+                        </Flex>
+                    </Flex>
+                </Box>
             </MainLayout>
         </>
     )
