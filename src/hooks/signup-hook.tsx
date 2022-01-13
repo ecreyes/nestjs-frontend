f2484ae584515dfd75cdc7ef0674188dae2b6e09
Eduardo Reyes <ereyesoyarzo@gmail.com>
2022-01-13 12:18:21 -0300

    handle errors signup

---
+++ src/hooks/signup-hook.tsx
@@ -0,0 +1,15 @@
+import { gql, useMutation } from "@apollo/client";
+
+const SIGN_UP = gql`
+mutation($input: SignUpInput!) {
+  signUp( input: $input){
+    id
+    name
+    email
+  }
+}`
+
+export const useSignUp = () => {
+    const [ makeSignUp, result ] = useMutation(SIGN_UP)
+    return { makeSignUp, result }
+}
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -6,18 +6,67 @@ import {
     AlertTitle, AlertDescription,
 } from '@chakra-ui/react'
 import ButtonRokket from "../components/ButtonRokket"
+import { useEffect, useState } from "react"
+import { useSignUp } from "../hooks/signup-hook"
+import { ApolloError } from "@apollo/client"
+
 
 const SignUp: NextPage = () => {
+    const { makeSignUp, result } = useSignUp()
+    const { data, loading, error} = result
+    const [showAlert, setShowAlert] = useState(false)
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMsg, setErrorMsg] = useState('')
+
+    useEffect(()=>{
+        setShowAlert(false)
+        if(data){
+            console.log({data})
+        }
+        if(error){
+            handleErrorMsg(error)
+        }
+    },[data, error, loading])
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const signUpInput = {
+           name: username,
+           email,
+           password
+        }
+        makeSignUp({
+            variables: {
+              input: signUpInput
+            }
+        })
+    }
+
+    const handleErrorMsg = (error:ApolloError) => {
+        setShowAlert(true)
+        const msg = error.message.toLowerCase()
+        if(msg.includes('bad')){
+            setErrorMsg('Ocurrió un problema al intentar registarse')
+        }
+        if(msg.includes('email')){
+            setErrorMsg('El email ya se encuentra en uso.')
+        }
+    }
+
+
     return (
-        <Flex justify="center" align='center' height='100vh' bg='#614ae0'>
-            <Flex direction={'column'} borderRadius={[null, 4]} width={['100%', 'md']} height={['100vh', '90vh']} bg='white'>
-                <Alert status='error' my={4} variant='top-accent'>
+        <Flex justify="center" align='center' height='100vh' bgGradient='linear(to-br, #614ae0, #9080e9)'>
+            <Flex direction={'column'} padding={[null,4]} boxShadow={[null,'md']} borderRadius={[null, 8]} width={['100%', 'md']} height={['100vh', '90vh']} bg='white'>
+                {showAlert && <Alert status='error' my={4} variant='top-accent'>
                     <AlertIcon />
                     <Flex direction={'column'}>
                         <AlertTitle mr={2}>Error:</AlertTitle>
-                        <AlertDescription>Your Chakra experience may be degraded.</AlertDescription>
+                        <AlertDescription>{errorMsg}</AlertDescription>
                     </Flex>
                 </Alert>
+                }
                 <Flex
                     height={'100%'}
                     direction='column'
@@ -26,23 +75,23 @@ const SignUp: NextPage = () => {
                     justify='space-evenly'
                 >
                     <Center>
-                        <Heading as='h1' size={'md'}>Bienvenid@!</Heading>
+                        <Heading as='h1' size={'md'} marginTop={[null,-8]}>Bienvenid@!</Heading>
                     </Center>
                     <Box>
-                        <FormControl marginBottom={4}>
+                        <FormControl marginBottom={4} isRequired isDisabled={loading}>
                             <FormLabel htmlFor='username'>Nombre de usuario:</FormLabel>
-                            <Input id='username' type='text' />
+                            <Input id='username' type='text' onChange={(e) => setUsername(e.target.value)} />
                         </FormControl>
-                        <FormControl marginBottom={4}>
+                        <FormControl marginBottom={4} isRequired isDisabled={loading}>
                             <FormLabel htmlFor='email'>Correo:</FormLabel>
-                            <Input id='email' type='email' />
+                            <Input id='email' type='email' onChange={(e) => setEmail(e.target.value)} />
                         </FormControl>
-                        <FormControl>
+                        <FormControl isRequired isDisabled={loading}>
                             <FormLabel htmlFor='password'>Contraseña:</FormLabel>
-                            <Input id='password' type='password' />
+                            <Input id='password' type='password' onChange={(e) => setPassword(e.target.value)} />
                         </FormControl>
                     </Box>
-                    <ButtonRokket>Registrarme</ButtonRokket>
+                    <ButtonRokket mt={[null,2]} onClick={handleSubmit} isLoading={loading} loadingText='Cargando...' isDisabled={loading}>Registrarme</ButtonRokket>
                 </Flex>
             </Flex>
         </Flex>
